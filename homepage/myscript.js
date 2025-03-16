068f2942b99a16e43b2360adf281dc1a8ea74344
Marcus Wunderlich <wunderlichmarcus@outlook.com>
2025-03-16 19:07:42 -0400

    Add new HTML page and Javascript

---
+++ homepage/myscript.js
@@ -0,0 +1,22 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+
+  document.getElementById("row-1").addEventListener("click", function() {
+    window.location.href = "index.html";}); 
+
+  document.getElementById("row-2").addEventListener("click", function() {
+    window.location.href = "sailing.html";}); 
+    
+  document.getElementById("row-3").addEventListener("click", function() {
+    window.location.href = "about.html";}); 
+    
+  document.getElementById("row-4").addEventListener("click", function() {
+    window.location.href = "cards.html";}); 
+  
+  });
+
+
+
+
+
+
